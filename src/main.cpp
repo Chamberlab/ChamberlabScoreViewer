9e8c694bffa9c991d23f4813a7beba3e793eec99
Anton Koch <anton.koch@gmail.com>
2016-12-15 18:12:13 +0100

    use game mode fullscreen on linux

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,20 @@
 
 //========================================================================
 int main( ){
+#ifdef __linux__
+	ofGLFWWindowSettings settings;
+	settings.multiMonitorFullScreen = true;
+	settings.windowMode = OF_GAME_MODE;
+	auto window = ofCreateWindow(settings);
+
+	ofRunApp(window, std::make_shared<ofApp>());
+	ofRunMainLoop();
+#else
 	ofSetupOpenGL(1280,720,OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
-
+#endif
 }
